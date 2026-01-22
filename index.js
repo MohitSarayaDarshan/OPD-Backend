3e8042b317ad2c296e5c12525a4cc0fdde45449d
MohitSaraya2005 <hhareshsaraya@gmail.com>
2026-01-22 19:39:54 +0530

    backend half complete-2

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const dotenv=require("dotenv")
 
 const hospitalRoutes=require("./routes/hospitalRoutes")
 const doctorRoutes=require("./routes/doctorRoutes")
+const diagnosisTypesRoutes=require("./routes/diagnosisTypesRoutes")
+const opdDiagnosisTypesRoutes=require("./routes/opdDiagnosisTypesRoutes")
+const opdRoutes=require("./routes/opdRoutes")
+const patientRoutes=require("./routes/patientRoutes")
+const receiptRoutes=require("./routes/receiptRoutes")
+const subtreatmentTypesRoutes=require("./routes/subTreamentTypesRoutes")
+const treatmentTypesRoutes=require("./routes/treatmentTypesRoutes")
 
 const connectDB = require("./config/db")
 
@@ -18,6 +25,20 @@ app.use(express.json())
 app.use('/api/hospitals',hospitalRoutes)
 
 app.use('/api/doctors',doctorRoutes)
+
+app.use('/api/diagnosistypes',diagnosisTypesRoutes)
+
+app.use('/api/opddiagnosistypes',opdDiagnosisTypesRoutes)
+
+app.use('/api/opds',opdRoutes)
+
+app.use('/api/patients',patientRoutes)
+
+app.use('/api/receipts',receiptRoutes)
+
+app.use('/api/subtreatments',subtreatmentTypesRoutes)
+
+app.use('/api/treatments',treatmentTypesRoutes)
 
 const PORT=process.env.PORT
 
